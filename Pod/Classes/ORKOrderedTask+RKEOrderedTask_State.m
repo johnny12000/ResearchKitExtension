0bb52af5af36fb81c03de5a2f9e3fb35867c96dd
Nikola Ristic <n.ristic@youngculture.com>
2015-07-23 10:58:58 +0200

    Syntax correction

--- Pod/Classes/ORKOrderedTask+RKEOrderedTask_State.m
+++ Pod/Classes/ORKOrderedTask+RKEOrderedTask_State.m
@@ -20,25 +20,24 @@
     id teststruct = @{ @"step" : @[@{ @"cond1": @"step2" }, @{@"cond2":@"step3"}],
                        @"step2" : @"step3" };
     
-    transitionTable enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
-        ORKStepNavigationRule* navigationRule = nil;
-        
-        if([obj isKindOfClass:[ORKStep class]]) {
-            ORKStep* nextStep = (ORKStep*)obj;
-            navigationRule = [[ORKDirectStepNavigationRule alloc] initWithDestinationStepIdentifier:nextStep.identifier];
-            
-        }
-        else {
-            NSArray* transitions = (NSArray*)obj;
-            
-            NSArray* predicates = @[  [NSPredicate predicateWithFormat:@"%K = %@" argumentArray:@[@"result", @"conditionvalue"]] ];
-            NSArray* states = @[];
-            
-            navigationRule = [[ORKPredicateStepNavigationRule alloc]initWithResultPredicates:predicates destinationStepIdentifiers:states];
-        }
-        
-        [result setNavigationRule:navigationRule forTriggerStepIdentifier:((ORKStep*)key).identifier];
-    }
+    [transitionTable enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+         ORKStepNavigationRule* navigationRule = nil;
+         
+         if([obj isKindOfClass:[ORKStep class]]) {
+             ORKStep* nextStep = (ORKStep*)obj;
+             navigationRule = [[ORKDirectStepNavigationRule alloc] initWithDestinationStepIdentifier:nextStep.identifier];
+         }
+         else {
+             NSArray* transitions = (NSArray*)obj;
+             
+             NSArray* predicates = @[  [NSPredicate predicateWithFormat:@"%K = %@" argumentArray:@[@"result", @"conditionvalue"]] ];
+             NSArray* states = @[];
+             
+             navigationRule = [[ORKPredicateStepNavigationRule alloc]initWithResultPredicates:predicates destinationStepIdentifiers:states];
+         }
+         
+         [result setNavigationRule:navigationRule forTriggerStepIdentifier:((ORKStep*)key).identifier];
+     }];
     
     return result;
 }
